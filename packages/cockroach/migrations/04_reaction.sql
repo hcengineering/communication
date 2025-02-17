c42690e3a3fd149e56f1833edf9adf14677afae0
Kristina Fefelova <kristin.fefelova@gmail.com>
2025-02-17 15:53:32 +0400

    Update db schema

--- packages/cockroach/migrations/04_reaction.sql
+++ packages/cockroach/migrations/04_reaction.sql
@@ -1,13 +1,11 @@
-DROP TABLE IF EXISTS c_reaction CASCADE;
-CREATE TABLE IF NOT EXISTS c_reaction
+CREATE TABLE IF NOT EXISTS communication.reaction
 (
-    message_id UUID         NOT NULL,
+    message_id INT8         NOT NULL,
     reaction   VARCHAR(100) NOT NULL,
     creator    VARCHAR(255) NOT NULL,
     created    TIMESTAMPTZ  NOT NULL DEFAULT now(),
 
-    PRIMARY KEY (message_id, creator, reaction),
-    FOREIGN KEY (message_id) REFERENCES c_message (id) ON DELETE CASCADE
+    PRIMARY KEY (message_id, creator, reaction)
 );
 
-CREATE INDEX IF NOT EXISTS reaction_message_idx ON c_reaction (message_id);
+CREATE INDEX IF NOT EXISTS reaction_message_idx ON communication.reaction (message_id);
